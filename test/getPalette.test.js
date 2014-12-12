b40ce71fb46aa314168b2b66f9792e6b258efefd
instancetype <ravinderssahni@gmail.com>
2014-12-11 23:37:11 -0500

    Add tests with before and afterEach hooks

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -3,18 +3,41 @@
  */
  /* jshint bitwise : true, eqeqeq : true, forin : true, noarg : true, noempty : true, nonew : true, laxbreak : true,
    asi : true, esnext : true, laxcomma : true, sub : true, browser : true, node : true, phantom : true */
-/* global describe, it */
+/* global describe, it, before, afterEach */
 'use strict';
 
 var getPalette = require('../lib/getPalette')
   , assert = require('assert')
   , fs = require('fs')
+  , path = require('path')
+
+  , configPath = path.join(process.cwd(), 'config.json')
+  , writeConfig
+
+writeConfig = function(configuration, cb) {
+  fs.writeFile(configPath, JSON.stringify(configuration), cb)
+}
 
 
 describe('getPalette', function() {
+  var config
 
-  it('should throw an error if the result is not an array', function() {
-    assert.throws(getPalette, /Palette is not an array/)
+  before(function(done) {
+    fs.readFile(configPath, function(err, data) {
+      config = JSON.parse(data.toString())
+      done()
+    })
+  })
+
+  afterEach(function(done) {
+    writeConfig(config, done)
+  })
+
+  it('should throw an error if the result is not an array', function(done) {
+    writeConfig({ palette : 'not an array' }, function(err) {
+      assert.throws(getPalette, /Palette is not an array/)
+      done()
+    })
   })
 
 
